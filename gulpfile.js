d83ae2d53547ffc444c3ff93aa1cd7ccbec3e7ac
Alexa fato <alexafato@MacBook-Pro-Alexa.local>
2020-09-16 16:58:52 +0300

    41

--- gulpfile.js
+++ gulpfile.js
@@ -10,15 +10,15 @@ let path={
     fonts: project_folder+'/fonts/',
   },
   src: {
-    html: source_folder+'/*.html',
-    css: source_folder+'/scss/style.sccs',
+    html: [source_folder+'/*.html', "!" + source_folder +'/_*.html'],
+    css: source_folder+'/scss/style.scss',
     js: source_folder+'/js/script.js',
     img: source_folder+'/img/**/*.{jpg,png,svg,gif,ico,webp}',
     fonts: source_folder+'/fonts/*.ttf',
   },
   watch: {
     html: source_folder+'/**/*.html',
-    css: source_folder+'/scss/**/*.sccs',
+    css: source_folder+'/scss/**/*.scss',
     js: source_folder+'/js/**/*.js',
     img: source_folder+'/img/**/*.{jpg,png,svg,gif,ico,webp}',
     
@@ -27,10 +27,13 @@ let path={
   clean:'./'+ project_folder +'/'
 }
 
-let { src, dest } =require('gulp'),
+let { src, dest } = require('gulp'),
    gulp = require('gulp'),
    browsersync = require('browser-sync').create(),
-   fileinclude = require('gulp-file-include')
+   fileinclude = require('gulp-file-include'),
+   del = require('del'),
+   scss = require('gulp-sass');
+
 
 
 function browserSync(params) {
@@ -42,6 +45,7 @@ function browserSync(params) {
      notify:false
   })
 }
+
 function html() {
   return src(path.src.html)
     .pipe(fileinclude())
@@ -49,14 +53,31 @@ function html() {
     .pipe(browsersync.stream())
 }
 
-function watchFiles(params) {
-   gulp.watch([path.watch.html], html)
+
+function css() {
+  return src(path.src.css)
+    .pipe(
+       scss({
+         outputStyle: "expanded"
+       })
+    )
+     .pipe(dest(path.build.css))
+     .pipe(browsersync.stream())
 }
 
-let build = gulp.series(html)
+function watchFiles(params) {
+   gulp.watch([path.watch.html], html)
+   gulp.watch([path.watch.css], css)
+}
+
+function clean(params) {
+  return del(path.clean);
+}
+
+let build = gulp.series(clean,  gulp.parallel(css, html))
 let watch = gulp.parallel(build,watchFiles,browserSync);
 
-
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
